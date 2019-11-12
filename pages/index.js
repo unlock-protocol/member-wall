f5aca0012f27e12e7b1419a2a1764f56717cc5bb
Julien Genestoux <julien.genestoux@gmail.com>
2019-11-12 16:56:57 -0500

    updated homepage

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import Head from "../components/head"
 import Nav from "../components/nav"
 
 const Home = () => {
-  return <a href="https://github.com/julien51/member-wall"></a>
+  return (
+    <p>
+      More info on <a href="https://github.com/julien51/member-wall">Github</a>.
+    </p>
+  )
 }
 
 export default Home
