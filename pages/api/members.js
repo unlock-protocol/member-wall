c79af269988666507cec7adf4cf61f6e9ecfadbe
Julien Genestoux <julien.genestoux@gmail.com>
2021-05-26 15:00:18 -0400

    adding support for multiple networks

--- pages/api/members.js
+++ pages/api/members.js
@@ -5,15 +5,19 @@ import Members from "../../src/Components/Members"
 
 module.exports = async (req, res) => {
   let locks = []
+  let network = 1 // defaults to mainnet
   if (req.query.locks) {
     locks = req.query.locks.split(",")
+  }
+  if (req.query.network) {
+    network = parseInt(req.query.network)
   }
   if (!locks.length) {
     return res.status("422").send("Please make sure you have `locks=0x123`")
   }
   const maxWidth = req.query.maxWidth || 800
   const maxHeight = req.query.maxHeight || 500
-  const members = await getMembers(locks)
+  const members = await getMembers(locks, network)
   const content = renderToString(
     <Members maxWidth={maxWidth} maxHeight={maxHeight} members={members} />
   )
--- src/getMembers.js
+++ src/getMembers.js
@@ -4,14 +4,21 @@ import query from "./locksMembersQuery"
 /**
  * Returns all of the keys for a given lock
  */
-export const getMembers = async locks => {
+export const getMembers = async (locks, network) => {
+  let graphUrl = "https://api.thegraph.com/subgraphs/name/unlock-protocol/unlock"
+  if (network === 4) {
+    graphUrl = "https://api.thegraph.com/subgraphs/name/unlock-protocol/unlock-rinkeby"
+  } else if (network === 100) {
+    graphUrl = "https://api.thegraph.com/subgraphs/name/unlock-protocol/xdai"
+  }
   const response = await fetch(
-    "https://api.thegraph.com/subgraphs/name/unlock-protocol/unlock",
+    graphUrl,
     {
       method: "POST",
       body: JSON.stringify({ query: query(locks) })
     }
   ).then(res => res.json())
+
   return response.data.locks
     .map(lock => {
       return lock.keys.map(key => {
